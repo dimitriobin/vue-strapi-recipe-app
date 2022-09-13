a1fe8d813ab39b7017f8fffa0155a1695cdddd82
Dimitri Obin <dimitriobin@gmail.com>
2022-09-13 23:14:18 +0200

    init vue front end

---
+++ frontend/src/views/TestView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="is-size-1 has-text-danger">{{ message }}</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HomeView extends Vue {
+  message = 'This is a test';
+}
+</script>
