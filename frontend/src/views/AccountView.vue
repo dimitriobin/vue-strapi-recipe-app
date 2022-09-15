d09fd40960e24a03c3a35ef3364e0b658b2d3a14
Dimitri Obin <dimitriobin@gmail.com>
2022-09-15 22:29:16 +0200

    remove unused vue template

---
+++ frontend/src/views/AccountView.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <h1 class="is-size-1 has-text-black">{{ message }}</h1>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AccountView extends Vue {
+  message = 'Account';
+}
+</script>
